eb720c88ea1348b9936842f27d5b22f9ce198571
kmr-rohit <rr7433446@gmail.com>
2024-08-30 03:57:56 +0530

    Commit

--- api/main.py
+++ api/main.py
@@ -255,5 +255,9 @@ async def ask_question(request: QuestionRequest):
         'response': smart_response
     }
 
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
 # To run the server, use the following command:
 # uvicorn main:app --reload
